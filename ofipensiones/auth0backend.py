e62d37db7bc2ff1f5a90e52395c25ac6e13cbcb7
eacelis <ea.celis@uniandes.edu.co>
2024-12-05 01:34:03 -0500

    auth0 correciones

--- ofipensiones/auth0backend.py
+++ ofipensiones/auth0backend.py
@@ -40,7 +40,7 @@ def getRole(request):
     user = request.user
     auth0user = user.social_auth.filter(provider="auth0")[0]
     accessToken = auth0user.extra_data['access_token']
-    url = "dev-7k53zhc6oxlqqlf2.us.auth0.com/userinfo"
+    url =  "https://dev-7k53zhc6oxlqqlf2.us.auth0.com/userinfo"
     headers = {'authorization': 'Bearer ' + accessToken}
     resp = requests.get(url, headers=headers)
     userinfo = resp.json()
